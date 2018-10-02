c5c7ecbfdcb0a5796aefc24094d32f9a3750ee07
romy1935 <43724105+romy1935@users.noreply.github.com>
2018-10-02 19:38:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,6 @@ async def on_message(message):
 
 @bot.command()
 async def git():
-    await bot.say("**The github repository of the bot:**\nhttps://www.github.com/Romy1935/bot")
+    await bot.say("**My Github:**\nhttps://www.github.com/Romy1935/bot")
     
 bot.run(token)
