f2f77733bf17ebb63b0914d8c4f427a9f6210eb2
romy1935 <43724105+romy1935@users.noreply.github.com>
2018-10-04 07:49:03 +0100

    Update Renge.py

--- data/Renge.py
+++ data/Renge.py
@@ -12,4 +12,11 @@ renge_images = ["https://i.imgur.com/Kev8f0x.jpg", "https://i.imgur.com/F8XWB4p.
                 "https://i.imgur.com/QQEKaeY.jpg", "https://i.imgur.com/LaHjDgN.jpg", "https://i.imgur.com/EAubf2y.jpg",
                 "https://i.imgur.com/tM91xLt.jpg", "https://i.imgur.com/1kxG1f8.jpg", "https://i.imgur.com/84llUuH.jpg",
                 "https://i.imgur.com/hytwVUc.jpg", "https://i.imgur.com/AMBcVJU.jpg", "https://i.imgur.com/oAZ7leL.jpg"
+                "https://i.imgur.com/pvoKWag.gifv", "https://i.imgur.com/m3NYwFY.gifv", "https://i.imgur.com/kiVX09M.gifv",
+                "https://i.imgur.com/XGDzls2.gifv", "https://i.imgur.com/sqhFRBm.gifv", "https://i.imgur.com/cTSnuRt.gifv",
+                "https://i.imgur.com/AgzzS38.gifv", "https://i.imgur.com/dcHM4JS.gifv", "https://i.imgur.com/t9VqE0I.gifv",
+                "https://i.imgur.com/lnM8oui.gifv", "https://i.imgur.com/NyUqSTP.gifv", "https://i.imgur.com/8xkzCNs.gifv",
+                "https://i.imgur.com/1WdgOUA.gifv", "https://i.imgur.com/78X4cDL.gifv", "https://i.imgur.com/L9rCI3h.gifv",
+                "https://i.imgur.com/LFyWV1U.gifv", "https://i.imgur.com/1zks052.gifv", "https://i.imgur.com/XeOEP63.gifv",
+                "https://i.imgur.com/oRoEJ3u.gifv"
                 ]
