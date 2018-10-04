19ed17362ba6d2385321e9ccb5547022593ec345
romy1935 <43724105+romy1935@users.noreply.github.com>
2018-10-04 17:10:57 +0100

    Update traps.py

--- data/traps.py
+++ data/traps.py
@@ -1,2 +1,6 @@
 
-trap_images = ["https://imgur.com/a/c8Pm7I0"]
+trap_images = ["https://i.imgur.com/L3aztMi.jpg","https://i.imgur.com/J8lOaAF.jpg","https://i.imgur.com/FwbFmTB.jpg",
+               "https://i.imgur.com/thDK3if.jpg","https://i.imgur.com/csMpkoB.jpg","https://i.imgur.com/EsrGVJp.jpg",
+               "https://i.imgur.com/lsjbjPx.jpg","https://i.imgur.com/WEqD10d.jpg","https://i.imgur.com/iVyJMrV.jpg",
+               "https://i.imgur.com/CAw5r2u.jpg","https://i.imgur.com/lwjqV3I.jpg"
+               ]
