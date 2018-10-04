dc3b0abe8231a965ab888bff2348c29cb8139d40
romy1935 <43724105+romy1935@users.noreply.github.com>
2018-10-04 07:27:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ async def ping():
     await bot.say(moo)
     
 @bot.command()
-async def renge():
+async def renchon():
     output = random.sample(renge_images, 1)
     em = discord.Embed(title="Nyanpasu~", colour=0xFBC1F0)
     em.set_image(url=output[0])
