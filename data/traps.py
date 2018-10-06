adc7b7a5677e634157db92ec436e315a3021e882
romy1935 <43724105+romy1935@users.noreply.github.com>
2018-10-06 08:07:28 +0100

    Delete traps.py

--- data/traps.py
+++
@@ -1,6 +0,0 @@
-
-trap_images = ["https://i.imgur.com/L3aztMi.jpg","https://i.imgur.com/J8lOaAF.jpg","https://i.imgur.com/FwbFmTB.jpg",
-               "https://i.imgur.com/thDK3if.jpg","https://i.imgur.com/csMpkoB.jpg","https://i.imgur.com/EsrGVJp.jpg",
-               "https://i.imgur.com/lsjbjPx.jpg","https://i.imgur.com/WEqD10d.jpg","https://i.imgur.com/iVyJMrV.jpg",
-               "https://i.imgur.com/CAw5r2u.jpg","https://i.imgur.com/lwjqV3I.jpg"
-               ]
